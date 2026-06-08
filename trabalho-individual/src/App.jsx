b54d27a15f77c4ab0f4e1021721d30825de42e49
zmatheus77 <matheusslahr@gmail.com>
2026-06-08 17:35:14 -0300

    Adiciona UseState e UseEffect + Lógica básica

--- trabalho-individual/src/App.jsx
+++ trabalho-individual/src/App.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import styles from "./App.module.css";
 import logo from "./assets/logo-white.png";
 
 function App() {
+  const emailCorreto = "matheus@email.com";
+  const senhaCorreta = "123456";
+
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [logar, setLogar] = useState(false);
+  const [mensagem, setMensagem] = useState("");
+
+  useEffect(() => {
+    if (logar) {
+      console.log("Email:", email);
+      console.log("Senha:", senha);
+
+      if (email === emailCorreto && senha === senhaCorreta) {
+        setMensagem("fazendo login");
+      } 
+      else {
+        setMensagem("credenciais inválidas");
+      }
+
+      setLogar(false);
+    }
+  }, [logar]);
+
   return (
     <main className={styles.container}>
       <section className={styles.left}>
@@ -15,31 +40,36 @@ function App() {
           Crie sua conta ou entre
         </h2>
 
-        <form className={styles.form}>
+        <div className={styles.form}>
           <input
             type="email"
             placeholder="Email"
             className={styles.input}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
 
           <input
             type="password"
             placeholder="Senha"
             className={styles.input}
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
           />
 
-          <button type="submit" className={styles.blueButton}>
+          <button
+            className={styles.blueButton}
+            onClick={() => setLogar(true)}
+          >
             Entrar
           </button>
 
-          <button type="button" className={styles.whiteButton}>
+          <button className={styles.whiteButton}>
             Cadastrar
           </button>
-        </form>
+        </div>
 
-        <p className={styles.terms}>
-          Ao continuar, você concorda com os Termos de Serviço e a Política de Privacidade.
-        </p>
+        {mensagem && (<p className={styles.status}>{mensagem}</p>)}
       </section>
     </main>
   );
